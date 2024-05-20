959ebeb4c69574432cb46d476a59071bcd443d06
Maryna Makas <152323803+marimaki0@users.noreply.github.com>
2024-05-21 01:39:40 +0200

    Update wczytywanie_grafu_z_pliku_tekstowego.py

--- Problem_1/Punkt_1/wczytywanie_grafu_z_pliku_tekstowego.py
+++ Problem_1/Punkt_1/wczytywanie_grafu_z_pliku_tekstowego.py
@@ -8,7 +8,7 @@ def read_graph_from_file(filename):
             graph[u][v] = capacity
     return graph
 
-# Przykładowe wywołanie funkcji
+#przykladowe wywolanie funkcji
 filename = 'graph.txt'
 graph = read_graph_from_file(filename)
 print("Graf, ktory jest pobrany z pliku:", graph)
