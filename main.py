bb2ed3b80cf646062ac30e4e071dc37ec4441836
Yauheni <luferchikz@gmail.com>
2024-06-10 21:36:35 +0200

    again merging

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import Problem_3.Part1.punkt1 as p3
 import Problem_3.Part2_V1.P1 as p3_v1
 import Problem_3.Part2_V2.p3 as p3_v2
 
-<<<<<<< HEAD
-otoczka = dict(p1.get_result())
-=======
+
 otoczka = list(p1.get_result())
->>>>>>> 062c4779f26b67d5e3cdf138ab5c4a49fa647392
 jasnosc = p3_v1.MakeRandomBrightness(len(otoczka))
 
 plaszczaki_lista = p3.generate_and_find_max_plaszczaki(otoczka)
