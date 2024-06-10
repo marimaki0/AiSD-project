e2d837c999e3e488e2111c9162f3ed60c71805a3
Yauheni <luferchikz@gmail.com>
2024-06-10 20:26:35 +0200

    Problem_3/Part_1 update

--- Problem_3/Part1/punkt1.py
+++ Problem_3/Part1/punkt1.py
@@ -1,20 +1,27 @@
 import random
 import numpy as np
 
-plaszczaki=[random.randint(0, 100) for _ in range(800)]
+def generate_and_find_max_plaszczaki(punkty_otoczki: int) -> list[int]:
+    plaszczaki=[random.randint(0, punkty_otoczki) for _ in range(800)]
 
-part_size=len(plaszczaki)//8 
-parts = np.array_split(plaszczaki, 8)
-max_numbers = [np.max(part) for part in parts]
+    parts = np.array_split(plaszczaki, 8)
+    max_numbers = [np.max(part) for part in parts]
 
-strazniki = {}
-counter=1
-for i in max_numbers:
-    strazniki[f"plasczak {counter}"]=i
-    counter+=1
+    return max_numbers
 
-for key, value in strazniki.items():
-    print(f"{key}: {value}")
+if __name__ == "__main__":
+        
+
+    max_numbers = generate_and_find_max_plaszczaki(100)
+    strazniki = {}
+    counter=1
+
+    for i in max_numbers:
+            strazniki[f"plasczak {counter}"]=i
+            counter+=1
+
+    for key, value in strazniki.items():
+        print(f"{key}: {value}")
 
 
 
