8e2fa0c35a9b45e711f52c6305eea0ef373ca9b8
Yauheni <luferchikz@gmail.com>
2024-06-10 20:35:01 +0200

    First general code

--- Problem_3/Part1/punkt1.py
+++ Problem_3/Part1/punkt1.py
@@ -7,11 +7,19 @@ def generate_and_find_max_plaszczaki(punkty_otoczki: int) -> list[int]:
     parts = np.array_split(plaszczaki, 8)
     max_numbers = [np.max(part) for part in parts]
 
+    strazniki = {}
+    counter=1
+
+    for i in max_numbers:
+            strazniki[f"plasczak {counter}"]=i
+            counter+=1
+
+    for key, value in strazniki.items():
+        print(f"{key}: {value}")
+
     return max_numbers
 
 if __name__ == "__main__":
-        
-
     max_numbers = generate_and_find_max_plaszczaki(100)
     strazniki = {}
     counter=1
---
+++ main.py
@@ -0,0 +1,4 @@
+import Problem_1.Punkt_2.otoczka_wypukla_grahama as p1
+import Problem_3.Part1.punkt1 as p3
+
+p3.generate_and_find_max_plaszczaki(p1.get_result())
