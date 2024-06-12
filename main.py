8abfbd0d4cce8a5ac5480e28f844ca579fd661a5
Yauheni <luferchikz@gmail.com>
2024-06-12 14:27:38 +0200

    MERGING AGAIN

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import Problem_3.Part2_V2.p3 as p3_v2
 
 otoczka = list(p1.get_result())
 jasnosc = p3_v1.MakeRandomBrightness(len(otoczka))
-
+lista_jasnosc = []
+for i in range(8):
+    lista_jasnosc.append(jasnosc)
+    print(lista_jasnosc[i])
 plaszczaki_lista = p3.generate_and_find_max_plaszczaki(otoczka)
 
 print("jasnosc: ", jasnosc)
@@ -14,5 +17,9 @@ print("otoczka: ", otoczka)
 print("plaszczaki lista: ", plaszczaki_lista)
 
 for i in range (0, len(plaszczaki_lista) - 1):
-    rez=jasnosc
-    print("Optimal stop: ", p3_v1.GetOptimalStops(rez, plaszczaki_lista[i]))
+    rez = jasnosc
+    en = plaszczaki_lista[i]
+    r = p3_v1.GetOptimalStops(rez, en)
+    print("Optimal stop v1: ", r)
+    r2 = p3_v2.GetOptimalStops(rez, en)
+    print("Optimal stop v2: ", r2)
