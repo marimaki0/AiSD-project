53c1c8950f458f92ddf7eee05a1232ed6c855e10
vollarai <raivolha@gmail.com>
2024-06-12 22:19:53 +0200

    test

--- Problem_1/Punkt_2/testy.py
+++ Problem_1/Punkt_2/testy.py
@@ -18,57 +18,46 @@ class Point:
     def __str__(self):
         return f"({self.x}, {self.y})"
 
-def Det(p1,p2,p3):
+def Det(p1, p2, p3):
     return p1.x * p2.y + p2.x * p3.y + p3.x * p1.y - p2.y * p3.x - p1.y * p2.x - p3.y * p1.x
 
 def Angle(point0, p):
     return math.atan2(p.y - point0.y, p.x - point0.x)
 
 def Graham(points):
-    point0=min(points, key=lambda p:(p.y, p.x))
+    point0 = min(points, key=lambda p: (p.y, p.x))
     points.remove(point0)
 
     points.sort(key=lambda p: (Angle(point0, p), point0.distance_between_2points(p)))
 
-    stack=[point0, points[0]]
+    stack = [point0, points[0]]
 
     for p in points[1:]:
-        while len(stack)>1 and Det(stack[-2], stack[-1], p) <= 0:
+        while len(stack) > 1 and Det(stack[-2], stack[-1], p) <= 0:
             stack.pop()
         stack.append(p)
 
     return stack
 
-def GeneratePoints(number, min_bound, max_bound):
+def read_points_from_file(filename):
     points = []
-    for _ in range(number):
-        x = random.randint(min_bound, max_bound)
-        y = random.randint(min_bound, max_bound)
-        points.append(Point(x, y))
-
-    for i in points:
-        print(i)
+    with open(filename, 'r') as file:
+        for line in file:
+            x, y = map(int, line.split())
+            points.append(Point(x, y))
     return points
 
-
-def GetResult():
-    points = GeneratePoints(20, -100, 100)
-    result = Graham(points)
-
-   # print("Punkty otoczki:")
-   # for point in result:
-   #     point.print()
-
-    return Graham(points)
-
+def GetResult(custom_points):
+    result = Graham(custom_points)
+    return result
 
 if __name__ == "__main__":
-    result = GetResult()
+    # Read points from file
+    filename = 'C:/Users/Comp/Desktop/projekt_aisd2/AiSD-project/Problem_1/Punkt_2/test1.txt'
+    custom_points = read_points_from_file(filename)
+
+    result = GetResult(custom_points)
 
     print("Punkty otoczki:")
     for point in result:
         point.print()
-
-
-
-
